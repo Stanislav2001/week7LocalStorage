075272c8f48cf7aa60299c4e7eaac445d1bba986
StanislavK007 <98669727+StanislavK007@users.noreply.github.com>
2022-03-12 19:16:19 +0200

    local storage

---
+++ scripts/app.js
@@ -0,0 +1,25 @@
+const container = document.querySelector('.container');
+const movieTitle = document.querySelector('.userInputTitle');
+const moviePosterUrl = document.querySelector('.userInputPosterUrl');
+const btn = document.querySelector('.button');
+const movieTitleToDisplay = document.querySelector('.favouriteMovieTitle');
+
+let titleInStorage = localStorage.getItem('title');
+let imageUrlInStorage = localStorage.getItem('imageUrl');
+
+if(titleInStorage && imageUrlInStorage) {
+    movieTitleToDisplay.textContent = titleInStorage;
+    container.style.backgroundImage = `linear-gradient(rgba(103, 103, 190, 0.664), rgba(48, 47, 47, 0.685)), 
+    url('${imageUrlInStorage}')`;
+}
+
+
+btn.addEventListener('click', () => {
+    let movieTitleInput = movieTitle.value;
+    let posterUrlInput = moviePosterUrl.value;
+    localStorage.setItem('title', movieTitleInput);
+    localStorage.setItem('imageUrl', posterUrlInput);
+    movieTitleToDisplay.textContent = movieTitleInput;
+    container.style.backgroundImage = `linear-gradient(rgba(103, 103, 190, 0.664), rgba(48, 47, 47, 0.685)),
+    url('${posterUrlInput}')`;
+});
